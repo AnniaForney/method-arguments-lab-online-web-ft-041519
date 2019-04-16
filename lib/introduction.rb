d7c9596c87452c9e7def8d8b533d40100ad0b99e
Annia Forney <anniaforney@gmail.com>
2019-04-16 03:32:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,11 @@
 
 
 def 
-introduction (name, language)
+  introduction (name)
+  puts "Hi, my name is #{name}."
+end 
+
+def 
+introduction with language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-  
